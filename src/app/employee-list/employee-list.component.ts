a263481500c04c0f04cabcccf83e8f1e96408c14
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-04-30 17:05:50 +0600

    Connecting Angular with List Employee REST API

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -8,21 +9,30 @@ import { Employee } from '../employee';
 })
 export class EmployeeListComponent {
 
-  employees : Employee[];
+  employees !: Employee[];
 
-  constructor () {
-    this.employees = [{
-      "id": 1,
-      "firstName": "Md. ",
-      "lastName": "Asad",
-      "emailId": "a@gmail.com"
-    },
-    {
-      "id": 2,
-      "firstName": "Md. ",
-      "lastName": "Akul",
-      "emailId": "ak@gmail.com"
-    }]
+  constructor (private employeeService: EmployeeService) {
+
+    // this.employees = [{
+    //   "id": 1,
+    //   "firstName": "Md. ",
+    //   "lastName": "Asad",
+    //   "emailId": "a@gmail.com"
+    // },
+    // {
+    //   "id": 2,
+    //   "firstName": "Md. ",
+    //   "lastName": "Akul",
+    //   "emailId": "ak@gmail.com"
+    // }]
+
+    this.getEmployees();
   };
+
+  private getEmployees() {
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    })
+  }
 
 }
