12a8d28c33d757de7e26e45e97ba229aaf26dc8c
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-02 12:52:58 +0600

    Angular Create Employee Form Handling

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,3 +1,4 @@
+import { Employee } from './../employee';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent {
+
+  employee : Employee = new Employee();
+
+  onSubmit() {
+    console.log(this.employee);
+  }
 
 }
