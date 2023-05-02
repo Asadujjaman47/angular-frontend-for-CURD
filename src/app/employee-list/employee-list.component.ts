e9fda6bc24298f3aea52fff1cac309a04535fbfc
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-02 15:15:03 +0600

    Creating Angular Update Employee Component

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -11,7 +12,8 @@ export class EmployeeListComponent {
 
   employees !: Employee[];
 
-  constructor (private employeeService: EmployeeService) {
+  constructor (private employeeService: EmployeeService,
+    private router: Router) {
 
     // this.employees = [{
     //   "id": 1,
@@ -34,5 +36,10 @@ export class EmployeeListComponent {
       this.employees = data;
     })
   }
+
+  updateEmployee(id: number) {
+    this.router.navigate(['update-employee', id]);
+  }
+
 
 }
